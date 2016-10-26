b6062d04cdf0ae5a16915d7861583dc5ce4406b2
iagolobla <iagolobla@PortatiLobla>
2016-10-26 13:11:44 +0200

    Creacion basica del constructor de edificio.

--- src/Entidades/Recurso.java
+++ src/Entidades/Recurso.java
@@ -12,7 +12,7 @@ package Entidades;
 public class Recurso {
     private final int DFLCANTIDAD = 100;
     
-    private String tipo;
+    private final String tipo;  //El tipo de una instancia no puede cambiar
     private int cantidad;
     
     public Recurso(String tipe){
@@ -27,4 +27,12 @@ public class Recurso {
                 break;
         }*/
     }
+    public Recurso(String tipe, int cantidad){
+        tipo = tipe;
+        this.cantidad = cantidad;
+    }
+    
+    public void setCantidad(int cantidad){  //Este metodo es para el caso en el que un personaje sin suficiente
+        this.cantidad = cantidad;           //capacidad de carga intenta recolectar un recurso, hay que mermar
+    }                                       //la cantidad de ese recurso, es decir, solo quitar la capRecoleccion
 }
