053163f099fd2d4d5c20ebf092d88ed8646baeac
iagolobla <iagolobla@PortatiLobla>
2016-12-07 11:19:48 +0100

    Creacion de paquetes y clases

---
+++ src/Juego/Juego.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+/**
+ *
+ * @author iagolobla
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+//Bienvenido al mejor juego de la historia, hecho de la mejor forma posible
+
+public class Juego {
+    
+    public Juego() {
+        boolean seguir = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al juego.");
+        int numciv;
+      
+        do {
+            System.out.println("Introduzca el numero de civilizaciones con el que desea jugar (maximo 3. Nombres de una palabra)");
+            numciv = scanner.nextInt();
+        } while (numciv > 3 || numciv < 1);
+        String nombre;
+        nombre = scanner.nextLine();//para coger el \0
+        
+        while (seguir) {
+            System.out.print("$ ");
+            String linea = scanner.nextLine();
+            String lineaLowerCase = linea.toLowerCase();    //Pasamos todo a minusculas
+            String[] comando = lineaLowerCase.split(" ");
+            if (comando.length > 0) {
+                switch (comando[0].toLowerCase()) {
+                    
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Juego();
+    }
+    
+}
