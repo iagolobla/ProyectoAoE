f599873edc0d92d770370c2547f486e090ce53e4
iagolobla <iagolobla@PortatiLobla>
2016-12-12 16:15:03 +0100

    Haciendo Celda.java

--- src/Juego/Celda.java
+++ src/Juego/Celda.java
@@ -11,17 +11,20 @@ import Personajes.Personaje;
 import Personajes.Grupo;
 import Edificios.Edificio;
 import Recursos.Contenedor;
+import Recursos.Pradera;
+
 /**
  *
  * @author iagolobla
  */
 public class Celda {
+
     private ArrayList<Personaje> personajes;
     private ArrayList<Grupo> grupos;
     private Edificio edificio;
-    private Contenedor recurso;
-    private Posicion pos;
-    HashMap<String, Boolean> visible;
+    private Contenedor contenedor;
+    private Posicion posicion;
+    private HashMap<String, Boolean> visible;
 
     public Celda(Posicion posicion) {
         if (posicion == null) {
@@ -31,10 +34,11 @@ public class Celda {
         personajes = new ArrayList<Personaje>();
         visible = new HashMap<String, Boolean>();
         grupos = new ArrayList<Grupo>();
-        pos = new Posicion(posicion);
+        this.posicion = new Posicion(posicion);
+        contenedor = new Pradera(this.posicion);
     }
 
-    public Celda(String tipo, Posicion posicion, String Nombre, String civilizacion) {
+    public Celda(Contenedor contenedor, Posicion posicion) {
         if (posicion == null) {
             System.out.println("ERROR EN LA POSICION ->NULL");
             return;
@@ -44,124 +48,36 @@ public class Celda {
         }
         personajes = new ArrayList<Personaje>();
         grupos = new ArrayList<Grupo>();
-        pos = new Posicion(posicion);
-        
-        switch (tipo) {
-            case "soldado":
-                personajes.add(new Personaje("soldado", Nombre, pos));
-                visible.put(civilizacion, Boolean.TRUE);
-                break;
-            case "paisano":
-                personajes.add(new Personaje("paisano", Nombre, pos));
-                visible.put(civilizacion, Boolean.TRUE);
-                break;
-            case "ciudadela":
-                edificio = new Edificio("ciudadela", pos, Nombre);
-                visible.put(civilizacion, Boolean.TRUE);
-                this.tipo = tipo;
-                break;
-            case "casa":
-                edificio = new Edificio("casa", pos, Nombre);
-                visible.put(civilizacion, Boolean.TRUE);
-                this.tipo = tipo;
-                break;
-            case "cuartel":
-                edificio = new Edificio("cuartel", pos, Nombre);
-                visible.put(civilizacion, Boolean.TRUE);
-                this.tipo = tipo;
-                break;
-            case "torre":
-                edificio = new Edificio("torre", pos, Nombre);
-                visible.put(civilizacion, Boolean.TRUE);
-                this.tipo = tipo;
-                break;
-            case "bosque":
-                recurso = new Recurso("bosque", 400, Nombre, pos);   //De momento dejaremos 200 por defecto
-                visible.put(civilizacion, Boolean.FALSE);
-                this.tipo = tipo;
-                break;
-            case "cantera":
-                recurso = new Recurso("cantera", 400, Nombre, pos);   //De momento dejaremos 200 por defecto
-                visible.put(civilizacion, Boolean.FALSE);
-                this.tipo = tipo;
-                break;
-            case "arbusto":
-                recurso = new Recurso("arbusto", 400, Nombre, pos);   //De momento dejaremos 200 por defecto
-                visible.put(civilizacion, Boolean.FALSE);
-                this.tipo = tipo;
-                break;
+        this.posicion = new Posicion(posicion);
+        this.contenedor = contenedor;
 
-            default:
-                System.out.println("Tipo mal introducido");
-
-        }
-    }
-
-    public void agrupar(Mapa mapa) {//PUEDE DAR PROBLEMAS DE ALIASING. CUANDO METAS UN PERSONAJES CON UN GRUPO EN LA CELDA
-        if (grupos.size() > 0) {
-            Grupo g;
-            int tam=grupos.size();
-            for (int i=0;i<tam;i++) {
-                g=grupos.get(0);
-                g.desagrupar(mapa);
-            }
-            String Name = "grupo-" + (mapa.getCivilizacion().getCantidades()[6] + 1);
-            mapa.getCivilizacion().getCantidades()[6]++;
-            Grupo group = new Grupo(personajes, Name, pos, mapa.getCivilizacion().getNombre());
-            grupos.add(group);
-            mapa.getCivilizacion().getGrupos().put(Name, group);
-            System.out.println("Se ha creado el " + Name + ". El grupo esta formado por:");
-            for (Personaje x : group.getPersonajes()) {
-                System.out.println(x.getNombre());
-            }
-
-        } else if (personajes.size() > 1) {
-            String Name = "grupo-" + (mapa.getCivilizacion().getCantidades()[6] + 1);
-            mapa.getCivilizacion().getCantidades()[6]++;
-            Grupo group = new Grupo(personajes, Name, pos, mapa.getCivilizacion().getNombre());
-            grupos.add(group);
-            mapa.getCivilizacion().getGrupos().put(Name, group);
-            System.out.println("Se ha creado el " + Name + ". El grupo esta formado por:");
-            for (Personaje x : group.getPersonajes()) {
-                System.out.println(x.getNombre());
-            }
-        } else {
-            System.out.println("No se puede crear el grupo, solo hay un personaje");
-        }
     }
 
     public ArrayList<Personaje> getPersonajes() {
-        if (personajes != null) {
-            return personajes;
-        }
-        return null;
+        return personajes;
     }
 
     public Edificio getEdificio() {
-        if (edificio != null) {
-            return edificio;
-        }
-        return null;
+        return edificio;
     }
 
-    public Recurso getRecurso() {
-        if (recurso != null) {
-            return recurso;
-        }
-        return null;
+    public Contenedor getContenedor() {
+        return contenedor;
     }
 
     public boolean isLibre() {
-        if (tipo.equals("Pradera")) {
-            return !(isPaisano() || isSoldado());
+        if (contenedor instanceof Pradera) {
+            if (personajes.size() == 0) {
+                return true;
+            }
         }
         return false;
     }
 
-    public boolean isPersonaje(){
-        if(this.isPaisano() || this.isSoldado()){
-            for(Personaje P : personajes){
-                if(P.isGrupo()){
+    public boolean isPersonaje() {
+        if (this.isPaisano() || this.isSoldado()) {
+            for (Personaje P : personajes) {
+                if (P.isGrupo()) {
                     return false;
                 }
             }
@@ -169,40 +85,21 @@ public class Celda {
         }
         return false;
     }
-    
-    public boolean isPaisano() {
-        int Tam = personajes.size();
-        if (Tam == 0) {
-            return false;
-        }
-        return personajes.get(Tam - 1).isPaisano();
-    }
 
-    public boolean isSoldado() {
-        int Tam = personajes.size();
-        if (Tam == 0) {
-            return false;
-        }
-        return personajes.get(Tam - 1).isSoldado();
-    }
-    
-    public boolean isGrupo(){
+    public boolean isGrupo() {
         return (grupos.size() > 0);
     }
 
     public boolean isRecurso() {
         return (recurso != null);
     }
-    
-
 
     public boolean isEdificio() {
         return (edificio != null);
     }
 
-
     public void quitarPersonaje(Personaje P) {
-        if (this.isPaisano()||this.isSoldado()) {
+        if (this.isPaisano() || this.isSoldado()) {
             if (personajes.size() > 1) {
                 personajes.remove(P);
             } else {
