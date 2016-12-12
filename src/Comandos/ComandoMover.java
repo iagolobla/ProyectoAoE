2a932359a15a1ef7c2c4e28e1fd2de7a48c2ed03
javier <javier@javier-Aspire-VN7-591G>
2016-12-12 19:45:50 +0100

    Merge origin/Version3 into Version3
    Conflicts:
    	src/Edificios/Cuartel.java

--- src/Comandos/ComandoMover.java
+++ src/Comandos/ComandoMover.java
@@ -5,12 +5,45 @@
  */
 package Comandos;
 
+import Personajes.Personaje;
+import Juego.Posicion;
+import Juego.Mapa;
+import Juego.Celda;
+import Edificios.Edificio;
 /**
  *
  * @author iagolobla
  */
-public class ComandoMover implements Comando{
-    public void ejecutar(){
-        
+public class ComandoMover implements Comando {
+
+    String direccion;
+    Personaje personaje;
+    Mapa mapa;
+
+    public ComandoMover(String direccion, Personaje personaje, Mapa mapa) {
+        if (mapa != null) {
+            this.mapa = mapa;
+        }
+        this.direccion = direccion;
+        if (personaje != null) {
+            this.personaje = personaje;
+        }
+
+    }
+
+    public void ejecutar() {
+        Posicion vieja = personaje.getPosicion();
+        Posicion nueva = personaje.mover(direccion);
+        Celda newcell=null;
+        Celda cell = mapa.getCelda(vieja);
+        if (mapa.checkBuilding(nueva) && mapa.checkCoords(nueva)) {
+            newcell=mapa.getCelda(nueva);
+        }else{
+            return;//excepcion
+        }
+        cell.quitarPersonaje(personaje);
+        newcell.addPersonaje(personaje);
+        personaje.setPosicion(nueva);
+        mapa.actualizarVisibilidad();
     }
 }
--- src/Juego/Juego.java
+++ src/Juego/Juego.java
@@ -9,22 +9,22 @@ package Juego;
  *
  * @author iagolobla
  */
-
+import Comandos.ComandoImprimir;
+import Comandos.ComandoMover;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
+import Personajes.Personaje;
 
 //Bienvenido al mejor juego de la historia, hecho de la mejor forma posible
-
 public class Juego {
-    
+
     public Juego() {
         ConsolaNormal Shell = new ConsolaNormal();
         boolean seguir = true;
         Shell.imprimir("Bienvenido al Juego!");
         int numciv;
-        
-        
+
         Civilizacion C;
         HashMap<String, Civilizacion> civilizaciones = new HashMap<String, Civilizacion>();
         do {
@@ -35,7 +35,7 @@ public class Juego {
         for (int i = 1; i <= numciv; i++) {
             nombre = Shell.leer("Introduzca el nombre de la civilizacion " + i + ": ");
             C = new Civilizacion(nombre);
-            
+
             if (i == 1) {
                 C.setColor("azul");
             } else if (i == 2) {
@@ -44,34 +44,42 @@ public class Juego {
                 C.setColor("morado");
             }
             civilizaciones.put(nombre, C);
-            
+
         }
-        
+
         C = civilizaciones.get(nombre);
-        
         System.out.println("Estas jugando con los: " + C.getNombre());
         Mapa map = new Mapa(6, 6, 6, civilizaciones.values());
         map.setCivilizacion(C);
-        map.print();
         
+
         while (seguir) {
             String linea = Shell.leer("$ ");
             String lineaLowerCase = linea.toLowerCase();    //Pasamos todo a minusculas
             String[] comando = lineaLowerCase.split(" ");
             if (comando.length > 0) {
                 switch (comando[0].toLowerCase()) {
+                    case "mover":
+                        if (C.getPersonajes().containsKey(comando[1])) {
+                            Personaje p = map.getCivilizacion().getPersonajes().get(comando[1]);
+                            new ComandoMover(comando[2],p,map).ejecutar();
+                            new ComandoImprimir(map).ejecutar();
+                        }
+                        break;
+                    case "imprimir":
+                        new ComandoImprimir(map).ejecutar();
+                        break;
                     case "salir":
                         seguir = false;
-                        map.print();
                         break;
                 }
             }
         }
     }
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
         new Juego();
     }
-    
+
 }
