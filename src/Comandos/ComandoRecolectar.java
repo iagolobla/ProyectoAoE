213bfe5319173171b75195dae7882014d83003c7
javier <javier@javier-Aspire-VN7-591G>
2016-12-15 17:04:19 +0100

    Merge origin/Version3 into Version3
    Conflicts:
    	src/Juego/Mapa.java

--- src/Comandos/ComandoRecolectar.java
+++ src/Comandos/ComandoRecolectar.java
@@ -13,6 +13,7 @@ import Juego.ConsolaNormal;
 import Juego.Mapa;
 import Juego.Posicion;
 import static Juego.Principal.SHELL;
+import Personajes.Grupo;
 import Personajes.Paisano;
 import Personajes.Personaje;
 import Recursos.Contenedor;
@@ -30,40 +31,46 @@ public class ComandoRecolectar implements Comando {
     private String direccion;
     private String personaje;
     private Mapa mapa;
-    
 
     public ComandoRecolectar(String direccion, String personaje, Mapa mapa) {
         this.direccion = direccion;
         this.personaje = personaje;
         this.mapa = mapa;
-        
+
     }
 
-    public void ejecutar() throws ExcepcionRecolectar, ExcepcionSintaxis, ExcepcionRecurso{
+    public void ejecutar() throws ExcepcionRecolectar, ExcepcionSintaxis, ExcepcionRecurso {
         Personaje P;
         Posicion cont;
         Celda cell;
         Contenedor contenedor;
-        
-        if (!mapa.getCivilizacion().getPersonajes().containsKey(personaje)){
+
+        if (!mapa.getCivilizacion().getPersonajes().containsKey(personaje)) {
             throw new ExcepcionRecolectar("El personaje indicado no existe!");
         }
         P = mapa.getCivilizacion().getPersonajes().get(personaje);
-        
+
         cont = P.mover(direccion);
-        
+
         cell = mapa.getCelda(cont);
         contenedor = cell.getContenedor();
-        
-        if(contenedor instanceof Pradera){
+
+        if (contenedor instanceof Pradera) {
             throw new ExcepcionRecolectar("No hay un recurso en la direccion especificada!");
         }
-        
+
         P.recolectar(contenedor);
-        if(contenedor.getRecurso().getCantidad()==0){
+        if (contenedor.getRecurso().getCantidad() == 0) {
             this.mapa.getRecursos().remove(contenedor.getNombre());
             contenedor.destruir(cell);
         }
-        SHELL.imprimir("El "+ P.getNombre() +" ahora tiene " + ((Paisano) P).getCantidadRecolectada() + " uds. de " + ((Paisano) P).getRecurso().getClass().getSimpleName());
+        if (P instanceof Grupo) {
+            for (Personaje p : ((Grupo) P).getPersonajes()) {
+                SHELL.imprimir("El " + p.getNombre() + " ahora tiene " + ((Paisano) p).getCantidadRecolectada() + " uds. de " + ((Paisano) p).getRecurso().getClass().getSimpleName());
+            }
+        } else {
+            SHELL.imprimir("El " + P.getNombre() + " ahora tiene " + ((Paisano) P).getCantidadRecolectada() + " uds. de " + ((Paisano) P).getRecurso().getClass().getSimpleName());
+
+        }
     }
 }
